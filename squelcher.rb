908937816b33c7693e140af0b758ef94a747a28e
Duncan Stuart <dgmstuart@gmail.com>
2017-02-18 22:41:08 +0000

    (refactor) inject a date parser

--- squelcher.rb
+++ squelcher.rb
@@ -4,11 +4,9 @@ require "pry"
 class Squelcher
   class ParseError < StandardError; end
 
-  SEPARATORS = /[\n,]/
-  DAYNAMES = Date::DAYNAMES + ["Mon", "Tues", "Tue", "Weds", "Wed", "Thurs", "Thur", "Thu", "Fri", "Sat", "Sun"]
-
-  def initialize(date_formatter = DateFormatter.new)
+  def initialize(date_formatter = DateFormatter.new, dates_parser = DatesParser.new)
     @date_formatter = date_formatter
+    @dates_parser = dates_parser
   end
 
   def squelch(input)
@@ -40,23 +38,36 @@ class Squelcher
   end
 
   def parse_input(input)
-    input = strip_daynames(input)
-    date_strings = input.split(SEPARATORS).map{ |s| s.strip }.reject(&:empty?)
-    parse_dates(date_strings)
+    @dates_parser.parse(input)
   end
 
-  def parse_dates(date_strings)
-    date_strings.map do |date_string|
+  class DatesParser
+    SEPARATORS = /[\n,]/
+    DAYNAMES = Date::DAYNAMES + ["Mon", "Tues", "Tue", "Weds", "Wed", "Thurs", "Thur", "Thu", "Fri", "Sat", "Sun"]
+
+    def parse(input)
+      date_strings = strip_daynames(input)
+        .split(SEPARATORS)
+        .map{ |s| s.strip }
+        .reject(&:empty?)
+      parse_dates(date_strings)
+    end
+
+    def parse_dates(date_strings)
+      date_strings.map { |date_string| parse_date(date_string) }
+    end
+
+    def parse_date(date_string)
       Chronic.parse(date_string, :endian_precedence => :little).tap do |date|
         raise ParseError, "Failed to parse \"#{date_string}\" as a date" if date.nil?
       end
     end
-  end
 
-  def strip_daynames(input)
-    output = input
-    DAYNAMES.each { |day| output = output.gsub(/#{day}/, '') }
-    output
+    def strip_daynames(input)
+      output = input
+      DAYNAMES.each { |day| output = output.gsub(day, '') }
+      output
+    end
   end
 
   class DateFormatter
